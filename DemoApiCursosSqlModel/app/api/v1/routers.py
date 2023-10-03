aa60c59b2cd79bda764a67f648f38d28c058d577
Lautaro Rataric <lautarorataric163@gmail.com>
2023-10-03 17:55:00 -0300

    Agregue archivos nuevos para reconocer modulos/se siguio modularizando

--- DemoApiCursosSqlModel/app/api/v1/routers.py
+++ DemoApiCursosSqlModel/app/api/v1/routers.py
@@ -1,3 +1,8 @@
 from fastapi import APIRouter
+from . import courses
+from . import users 
 
 router = APIRouter()
+
+router.include_router(users.router, prefix="/users", tags=["Users"])
+router.include_router(courses.router, prefix="/courses", tags=["Courses"])
