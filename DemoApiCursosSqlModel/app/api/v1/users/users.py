005ee1901911ef512a077cdfb9f3d4587791cf25
Lautaro Rataric <lautarorataric163@gmail.com>
2023-10-05 20:48:42 -0300

    operations crud complete model user

--- DemoApiCursosSqlModel/app/api/v1/users/users.py
+++ DemoApiCursosSqlModel/app/api/v1/users/users.py
@@ -7,7 +7,6 @@ from app.models.users.user import User, UserBase, UserCreate, UserRead, UserUpda
 
 router = APIRouter()
 
-
 @router.post("/", response_model=UserRead)
 async def create_user(*, session: Session = Depends(get_session), data: UserCreate):
 
@@ -25,4 +24,62 @@ async def get_users(*, session: Session = Depends(get_session)):
     with session:
         users = session.exec(select(User)).all()
         return users
+
+
+@router.get("/limit", response_model=list[UserRead])
+async def get_users_limit(*, offset: int = 0, limit: int = 10, session: Session = Depends(get_session)): 
+    
+    with session:
+        users = session.exec(select(User).offset(offset).limit(limit)).all()
+        return users   
+
+
+@router.get("/find/{id}", response_model=UserRead)
+async def find_user(id: int, session: Session = Depends(get_session)): 
+    
+    with session:
+        users = session.exec(select(User).where(User.id == id)).first()
+        return users    
+
+    
+@router.get("/find/{username}", response_model=UserRead)
+async def find_user_name(username: str, session: Session = Depends(get_session)): 
+    
+    with session:
+        users = session.exec(select(User).where(User.username == username)).first()
+        return users  
+    
+
+@router.put("/{id}", response_model=UserRead)
+async def update_user_by_id(id: int, updated_user: UserUpdate, session: Session = Depends(get_session)):
    
+    with session:
+        user = session.get(User, id)
+        if user is None:
+            raise HTTPException(status_code=404, detail="User not found")
+        
+        # Actualiza los campos según los valores proporcionados
+        user.full_name = updated_user.full_name
+        user.username = updated_user.username
+        user.email = updated_user.email
+        user.password = updated_user.password
+    
+        session.add(user)
+        session.commit()
+        session.refresh(user)
+        
+        return user
+
+
+@router.delete("/{id}")
+async def delete_user_by_id(id: int,  session: Session = Depends(get_session)):
+   
+    with session:
+        user = session.get(User, id)
+        if user is None:
+            raise HTTPException(status_code=404, detail="User not found")
+        
+        session.delete(user)
+        session.commit()
+        
+        return {"message": f"User with ID {id} deleted successfully"}    
