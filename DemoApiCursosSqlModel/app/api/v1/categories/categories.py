9dac69324e8eb1ee127f4b697e47c64e146059ed
Lautaro Rataric <lautarorataric163@gmail.com>
2023-10-06 01:35:14 -0300

    aggregate model categories/migrations and crud operations of the same

---
+++ DemoApiCursosSqlModel/app/api/v1/categories/categories.py
@@ -0,0 +1,81 @@
+from fastapi import APIRouter, Depends, HTTPException
+from sqlmodel import Session, select
+
+from app.db.database import get_session
+from app.models.categories.category import Categories, CategoriesCreate, CategoriesRead, CategoryeBase, CategoryUpdate 
+
+router = APIRouter()
+
+@router.post("/", response_model=CategoriesRead)
+async def create_category(*, session: Session = Depends(get_session), data: CategoriesCreate):
+
+    with session: 
+        new_category = Categories.from_orm(data)
+        session.add(new_category)
+        session.commit()
+        session.refresh(new_category)
+        return new_category
+
+
+@router.get("/", response_model=list[CategoriesRead])
+async def get_categories(*, session: Session = Depends(get_session)): 
+    
+    with session:
+        categories = session.exec(select(Categories)).all()
+        return categories
+
+
+@router.get("/limit", response_model=list[CategoriesRead])
+async def get_categories_limit(*, offset: int = 0, limit: int = 10, session: Session = Depends(get_session)): 
+    
+    with session:
+        categories = session.exec(select(Categories).offset(offset).limit(limit)).all()
+        return categories   
+
+
+@router.get("/find/{id}", response_model=CategoriesRead)
+async def find_category(id: int, session: Session = Depends(get_session)): 
+    
+    with session:
+        category = session.exec(select(Categories).where(Categories.id == id)).first()
+        return category    
+
+    
+@router.get("/find/{name}", response_model=CategoriesRead)
+async def find_category_name(name: str, session: Session = Depends(get_session)): 
+    
+    with session:
+        category = session.exec(select(Categories).where(Categories.name == name)).first()
+        return category  
+    
+
+@router.put("/{id}", response_model=CategoriesRead)
+async def update_category_by_id(id: int, updated_category: CategoryUpdate, session: Session = Depends(get_session)):
+   
+    with session:
+        category = session.get(Categories, id)
+        if category is None:
+            raise HTTPException(status_code=404, detail="Category not found")
+        
+        # Actualiza los campos según los valores proporcionados
+        category.name = updated_category.name
+        
+        session.add(category)
+        session.commit()
+        session.refresh(category)
+        
+        return category
+
+
+@router.delete("/{id}")
+async def delete_category_by_id(id: int,  session: Session = Depends(get_session)):
+   
+    with session:
+        category = session.get(Categories, id)
+        if category is None:
+            raise HTTPException(status_code=404, detail="Category not found")
+        
+        session.delete(category)
+        session.commit()
+        
+        return {"message": f"Category with ID {id} deleted successfully"}    
---
+++ DemoApiCursosSqlModel/app/models/categories/category.py
@@ -0,0 +1,24 @@
+from typing import Optional
+from sqlmodel import SQLModel, Field
+
+#Model
+class CategoryeBase(SQLModel):
+    name: str = Field(index=True, title="Category name ", max_length=254)
+    
+    
+#Hereda del modelo, schemas
+class Categories(CategoryeBase, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+
+
+class CategoriesCreate(CategoryeBase):
+    pass
+
+# schema para devolver los datos o verlos ---> response model
+class CategoriesRead(CategoryeBase):
+    id: int
+
+
+class CategoryUpdate(SQLModel):
+    name: Optional[str] = None
+   
